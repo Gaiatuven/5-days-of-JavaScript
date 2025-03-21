9c3aac8ac3e32b3d0fbe2440b08ea9c171b937d4
GaiaTuven <ggwiese@gmail.com>
2025-03-21 12:37:43 +0200

    refactored code into a expense function call

--- day-1/big-project/personal/main.js
+++ day-1/big-project/personal/main.js
@@ -1,56 +1,73 @@
 const prompt = require('prompt-sync')();
 
-let monthlyIncome = 0;
+// let monthlyIncome = 0;
 
-function calculateTotalIncome() {
-    let monthlyIncome = 0;
-    let income = Number(prompt('Enter monthly income: '));
+// function calculateTotalIncome() {
+//     let monthlyIncome = 0;
+//     let income = Number(prompt('Enter monthly income: '));
 
-    if (Number.isNaN(income)) {
-        console.log('Please enter a valid amount.');
-        return 0;
-    } else {
-        monthlyIncome += income;
-        let otherIncome = prompt('Do you have any other income (yes/no): ');
+//     if (Number.isNaN(income)) {
+//         console.log('Please enter a valid amount.');
+//         return 0;
+//     } else {
+//         monthlyIncome += income;
+//         let otherIncome = prompt('Do you have any other income (yes/no): ');
 
-        if (otherIncome.toLowerCase() === 'yes') {
-            let extraIncome = Number(prompt('Enter additional income: '));
-            if (Number.isNaN(extraIncome)) {
-                console.log('Enter a valid amount.');
-                return monthlyIncome; //return the current monthly income.
-            } else {
-                monthlyIncome += extraIncome;
-            }
+//         if (otherIncome.toLowerCase() === 'yes') {
+//             let extraIncome = Number(prompt('Enter additional income: '));
+//             if (Number.isNaN(extraIncome)) {
+//                 console.log('Enter a valid amount.');
+//                 return monthlyIncome; //return the current monthly income.
+//             } else {
+//                 monthlyIncome += extraIncome;
+//             }
+//         }
+//     }
+//     return monthlyIncome;
+// }
+
+// let result = calculateTotalIncome();
+// console.log(`Total income: $${result}`);
+
+
+
+// const fixMonthlyExpense = {
+//   rent: 3500,
+//   electricity: 1000,
+//   houseEquity: 1800,
+//   personalLoans: 1000,
+//   loans: 4780,
+
+//   deductFixedMonthlyExpenses: function() {
+//       for (const expense in this) {
+//           if (typeof this[expense] === 'number') { // Check if it's a number (expense value)
+//               monthlyIncome -= this[expense];
+//               console.log(`Deducted ${expense}: $${this[expense]}`);
+//           }
+//       }
+//       console.log(`Remaining income: $${monthlyIncome}`);
+//   }
+// };
+
+// fixMonthlyExpense.deductFixedMonthlyExpenses();
+
+
+function monthlyExpenses() {
+    let monthlyExpenses = 0; // Corrected typo
+    while (true) {
+        let input = prompt('Enter a monthly expense (or "done" to finish): ');
+        if (input.toLowerCase() === 'done') {
+            break; // Exit the loop
+        }
+        let monthlyExpense = Number(input);
+        if (isNaN(monthlyExpense)) {
+            console.log('Please enter a monthly expense in numerical format');
+        } else {
+            monthlyExpenses += monthlyExpense;
         }
     }
-    return monthlyIncome;
+    return monthlyExpenses;
 }
 
-let result = calculateTotalIncome();
-console.log(`Total income: $${result}`);
-
-
-
-const fixMonthlyExpense = {
-  rent: 3500,
-  electricity: 1000,
-  houseEquity: 1800,
-  personalLoans: 1000,
-  loans: 4780,
-
-  deductFixedMonthlyExpenses: function() {
-      for (const expense in this) {
-          if (typeof this[expense] === 'number') { // Check if it's a number (expense value)
-              monthlyIncome -= this[expense];
-              console.log(`Deducted ${expense}: $${this[expense]}`);
-          }
-      }
-      console.log(`Remaining income: $${monthlyIncome}`);
-  }
-};
-
-fixMonthlyExpense.deductFixedMonthlyExpenses();
-
-
-
-
+let result = monthlyExpenses();
+console.log(result);
