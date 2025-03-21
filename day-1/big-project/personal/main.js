07384119cfc472efde5620db19516f62aa48f5c3
GaiaTuven <ggwiese@gmail.com>
2025-03-21 10:46:11 +0200

    Added for in loop to over expense object

--- day-1/big-project/personal/main.js
+++ day-1/big-project/personal/main.js
@@ -1,5 +1,7 @@
 const prompt = require('prompt-sync')();
 
+let monthlyIncome = 0;
+
 function calculateTotalIncome() {
     let monthlyIncome = 0;
     let income = Number(prompt('Enter monthly income: '));
@@ -27,47 +29,28 @@ function calculateTotalIncome() {
 let result = calculateTotalIncome();
 console.log(`Total income: $${result}`);
 
-// const fixMonthlyExpense = {
-//     rent: 3500,
-//     electricity: 1000,
-//     houseEquity: 1800,
-//     personalLoans: 1000,
-//     loans: 4780,
 
-//     deductFixedMonthlyExpenses: function() {
-//         for (const expense in fixMonthlyExpense) {
 
-//         }
-//     }
-// };
+const fixMonthlyExpense = {
+  rent: 3500,
+  electricity: 1000,
+  houseEquity: 1800,
+  personalLoans: 1000,
+  loans: 4780,
 
-// function monthlyExpenses() {
-//   const fixExpenses = fixMonthlyExpense.totalDebit(); // Corrected object name
-//   let currentExpense = true;
-//   let expenseList = [];
+  deductFixedMonthlyExpenses: function() {
+      for (const expense in this) {
+          if (typeof this[expense] === 'number') { // Check if it's a number (expense value)
+              monthlyIncome -= this[expense];
+              console.log(`Deducted ${expense}: $${this[expense]}`);
+          }
+      }
+      console.log(`Remaining income: $${monthlyIncome}`);
+  }
+};
 
-//   while (currentExpense) {
-//     const userInput = prompt('Enter Expense for the month: ');
+fixMonthlyExpense.deductFixedMonthlyExpenses();
 
-//     if (userInput.toLowerCase() === "done") {
-//       currentExpense = false; // Set the flag to exit the loop
-//       console.log("Exiting loop.");
-//       expenseList.push(userInput);
-//       console.log(expenseList);
-//     } else {
-//       // Process the user's input here
-//       expenseList.push(userInput);
-//       console.log
-//       // You can add more logic here based on the user's input
-//     }
-//   }
-// }
 
-// let result = monthlyExpenses();
-// console.log(`Monthly Expenses: ${result}`); // Removed extra comma
 
-// // Example usage:
-// const totalExpenses = fixMonthlyExpense.totalDebit();
-// console.log(totalExpenses);
-// console.log(`Total Income: ${incomeSources()}`); // Added template literal.
-// console.log(expenseList)
+
